2c110820801941e45e7519f1ec5fcaef5d54751d
David Dahlgren <ddaviddahlgren@gmail.com>
2025-09-30 18:19:24 +0200

    js for startpage

---
+++ apps.js
@@ -0,0 +1,50 @@
+// Startsidan
+
+const mainBody = document.getElementById('body')
+const introBox = document.getElementById("intro-box");
+
+function startPageLoad () {
+    introBox.classList.add("content-fade-in");
+    introBox.addEventListener("fully-faded-in", () => { mainBody.appendChild(introBox); }, { once: true });
+}
+
+startPageLoad();
+
+const logoType = document.getElementById("shine-text")
+logoType.textContent = "Geekqwizz"
+
+
+const introText = document.createElement('p')
+introText.id = "intro-text"
+const introContent = "Welcome to Geekqwizz, a retro-style quiz-game! Choose between two categories and try to beat your high score!"
+
+const startButton = document.createElement('p')
+startButton.id = "start-button"
+
+startButton.textContent = "Click here to start!"
+
+const creatorsText1 = document.createElement('p');
+creatorsText1.id ="creators-text"
+const creatorsText2 = document.createElement('p');
+creatorsText2.id ="creators-text"
+
+const creatorsContent1 = "Created by:";
+const creatorsContent2 = "David, Desirée, Johan & Taif!";
+
+introText.textContent = introContent
+creatorsText1.textContent = creatorsContent1
+creatorsText2.textContent = creatorsContent2
+introBox.appendChild(introText);
+
+introBox.appendChild(startButton);
+
+introBox.appendChild(creatorsText1);
+introBox.appendChild(creatorsText2);
+
+function launchGame () {
+    introBox.classList.add("content-fade-out");
+    introBox.addEventListener("fully-faded-out", () => { introBox.remove(); }, { once: true });
+}
+
+startButton.addEventListener('click', launchGame);
+
